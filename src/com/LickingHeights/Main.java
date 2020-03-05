7141c95fed08803817a56e2b796272b345cf51aa
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-03-05 12:13:45 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,29 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+
     }
+
+    public static void testOddness() {
+    Scanner testOddness;
+    testOddness = new Scanner(System.in);
+            
+    System.out.println("Give me a number that is positive");
+
+int number = testOddness.nextInt();
+
+System.out.println(number + " is odd: " +(number % 2 !=0 ));
+
+
+
+
+
+
+    }
+
 }
